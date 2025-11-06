a8756b486cba0871e3a6ce2c14e77da9e5013b52
Santosh-7124 <sunny962002@gmail.com>
2025-11-06 13:03:51 +0530

    Gallery imgs added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,57 @@
-import React from "react";
-import AboutTestImg from "../assets/Gallery/Talankey aboutTestImg.jfif";
-import AboutTestImg1 from "../assets/Gallery/Talankey aboutTestImg1.jfif";
-import AboutTestImg2 from "../assets/Gallery/Talankey aboutTestImg2.jfif";
-import AboutTestImg3 from "../assets/Gallery/Talankey aboutTestImg3.jfif";
-import AboutTestImg4 from "../assets/Gallery/Talankey aboutTestImg4.jfif";
-import AboutTestImg5 from "../assets/Gallery/Talankey aboutTestImg5.jfif";
-import AboutTestImg6 from "../assets/Gallery/Talankey aboutTestImg6.jfif";
-import AboutTestImg7 from "../assets/Gallery/Talankey aboutTestImg7.jfif";
+import React, { useEffect, useState } from "react";
+
+import GalleryImage1 from "../assets/Gallery/Gallery Image 1.jpg";
+import GalleryImage2 from "../assets/Gallery/Gallery Image 2.jpg";
+import GalleryImage3 from "../assets/Gallery/Gallery Image 3.jpg";
+import GalleryImage4 from "../assets/Gallery/Gallery Image 4.jpg";
+import GalleryImage5 from "../assets/Gallery/Gallery Image 5.jpg";
+import GalleryImage6 from "../assets/Gallery/Gallery Image 6.jpg";
+import GalleryImage7 from "../assets/Gallery/Gallery Image 7.jpg";
+import GalleryImage8 from "../assets/Gallery/Gallery Image 8.jpg";
+import GalleryImage9 from "../assets/Gallery/Gallery Image 9.jpg";
+import GalleryImage10 from "../assets/Gallery/Gallery Image 10.jpg";
+import GalleryImage11 from "../assets/Gallery/Gallery Image 11.jpg";
+import GalleryImage12 from "../assets/Gallery/Gallery Image 12.jpg";
+import GalleryImage13 from "../assets/Gallery/Gallery Image 13.jpg";
+import GalleryImage14 from "../assets/Gallery/Gallery Image 14.jpg";
+import GalleryImage15 from "../assets/Gallery/Gallery Image 15.jpg";
+import GalleryImage16 from "../assets/Gallery/Gallery Image 16.jpg";
+import GalleryImage17 from "../assets/Gallery/Gallery Image 17.jpg";
+
+const allImages = [
+  GalleryImage1,
+  GalleryImage2,
+  GalleryImage3,
+  GalleryImage4,
+  GalleryImage5,
+  GalleryImage6,
+  GalleryImage7,
+  GalleryImage8,
+  GalleryImage9,
+  GalleryImage10,
+  GalleryImage11,
+  GalleryImage12,
+  GalleryImage13,
+  GalleryImage14,
+  GalleryImage15,
+  GalleryImage16,
+  GalleryImage17,
+];
 
 function Gallery() {
+  const [images, setImages] = useState([]);
+
+  const shuffleImages = () => {
+    const shuffled = [...allImages].sort(() => Math.random() - 0.5);
+    setImages(shuffled.slice(0, 8)); 
+  };
+
+  useEffect(() => {
+    shuffleImages();
+    const interval = setInterval(shuffleImages, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="infoContainer">
       <div className="infoContainerHeading">
@@ -25,24 +68,29 @@ function Gallery() {
           </p>
         </div>
       </div>
-      <div className="ourWork">
-        <div className="ourWorkColumn1">
-          <img src={AboutTestImg1}></img>
-          <img src={AboutTestImg2}></img>
-          <img src={AboutTestImg}></img>
-        </div>
-        <div className="ourWorkColumn2">
-          <img src={AboutTestImg3}></img>
-          <div className="ourWorkColumn2Sub">
-            <img src={AboutTestImg4}></img>
-            <img src={AboutTestImg5}></img>
+
+      {images.length > 0 && (
+        <div className="ourWork">
+          <div className="ourWorkColumn1">
+            <img src={images[0]} alt="" />
+            <img src={images[1]} alt="" />
+            <img src={images[2]} alt="" />
+          </div>
+
+          <div className="ourWorkColumn2">
+            <img src={images[3]} alt="" />
+            <div className="ourWorkColumn2Sub">
+              <img src={images[4]} alt="" />
+              <img src={images[5]} alt="" />
+            </div>
+          </div>
+
+          <div className="ourWorkColumn3">
+            <img src={images[6]} alt="" />
+            <img src={images[7]} alt="" />
           </div>
         </div>
-        <div className="ourWorkColumn3">
-          <img src={AboutTestImg6}></img>
-          <img src={AboutTestImg7}></img>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
